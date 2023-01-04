3975e7661017946802a32b192228fd2ce58a4577
Sagar Adval <sagaradval@gmail.com>
2023-01-04 17:41:03 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
 nvm install 16
 
 #create our working directory if it doesnt exist
