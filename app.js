9dcc1f5320b5075769de141b699bdfd7bde697ab
Sagar Adval <sagaradval@gmail.com>
2023-01-04 15:57:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 98</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
-      productId: '101',
-      price: 100
+      productId: '686701',
+      price: 1009
     },
     {
       productId: '102',
